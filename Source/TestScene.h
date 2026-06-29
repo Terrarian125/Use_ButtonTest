7b47f02e5ad97201111f5da0d5cb813e383ace1e
正人 髙橋 <240169@jc-21.jp>
2026-06-29 17:02:18 +0900

    設定と終了機能

--- Source/TestScene.h
+++ Source/TestScene.h
@@ -3,7 +3,11 @@
 #include "../Library/GuiButton.h"
 #include <vector>
 
+// クラスの前方宣言
+class SettingPanel;
+
 /// <summary>
+/// テストシーンクラス
 /// </summary>
 class TestScene : public SceneBase
 {
@@ -16,6 +20,13 @@ public:
 
 private:
     int bgImage;                     // 背景画像のグラフィックハンドル
-    int btnImage;                    // ボタン画像のグラフィックハンドル
-    std::vector<GuiButton*> buttons; // ボタンを管理する動的配列
+    std::vector<GuiButton*> buttons; // メインメニューのボタン配列
+
+    // 設定パネル関連
+    SettingPanel* mySettingPanel;    // 設定パネル
+
+    // 終了確認ダイアログ関連
+    bool isExitDialogVisible;        // ダイアログの表示フラグ
+    int currentSelect;               // キーボード選択用インデックス
+    std::vector<GuiButton*> exitButtons; // ダイアログ内のボタン配列（はい／いいえ）
 };
